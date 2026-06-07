415026c18effda8e4af6abe2bfb54b772e727e8d
한성길 <zaien24@gmail.com>
2026-06-07 18:37:56 +0900

    test

--- src/main/kotlin/com/seotae/start_one/StartOneApplication.kt
+++ src/main/kotlin/com/seotae/start_one/StartOneApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class StartOneApplication
 
+
+// test
 fun main(args: Array<String>) {
 	runApplication<StartOneApplication>(*args)
 }
