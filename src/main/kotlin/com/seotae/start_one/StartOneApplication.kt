4087e897ad9c989fc451851e36623445e4f7876f
한성길 <zaien24@gmail.com>
2026-06-07 11:53:27 +0900

    first commit

---
+++ src/main/kotlin/com/seotae/start_one/StartOneApplication.kt
@@ -0,0 +1,11 @@
+package com.seotae.start_one
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StartOneApplication
+
+fun main(args: Array<String>) {
+	runApplication<StartOneApplication>(*args)
+}
